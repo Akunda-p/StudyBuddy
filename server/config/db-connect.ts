fb552d7e1df0536ebfd4c73f17d8b425e5e359ab
Cody <cody.napersonal@gmail.com>
2023-11-14 15:22:01 +1300

    a

--- server/config/db-connect.ts
+++ server/config/db-connect.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const DEFAULT_CONNECTION_STRING: string = 'mongodb://127.0.0.1:27017/studyBuddy';
+const DEFAULT_CONNECTION_STRING: string = 'mongodb://mongo:-6b23fd1-FeB5eebG-G3cH5hEAHg543D@monorail.proxy.rlwy.net:17321/studyBuddy';
 
 // Connect to mongoDB with the connection string given as argument
 export default function connectToDatabase(connectionString: string = DEFAULT_CONNECTION_STRING) {
