9ad565ce7c3713d8bc1f3b40feff89737db840b4
Akunda-p <82128923+Akunda-p@users.noreply.github.com>
2024-01-30 16:09:05 +1300

    Update db-connect.ts

--- server/config/db-connect.ts
+++ server/config/db-connect.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const DEFAULT_CONNECTION_STRING: string = 'mongodb://mongo:tj7zOHSlHTJFYRPUaRti@containers-us-west-15.railway.app:6765';
+const DEFAULT_CONNECTION_STRING: string = 'mongodb://mongo:b26D6gDb5CgC1dhdE-ceEbbbG-24b262@monorail.proxy.rlwy.net:20770';
 
 // Connect to mongoDB with the connection string given as argument
 export default function connectToDatabase(connectionString: string = DEFAULT_CONNECTION_STRING) {
